95fd875b0c5f3b3eb3e1bd0975c41d1a38b3a53f
jovinds <jovinds24@gmail.com>
2025-01-03 21:59:07 +0800

    node

---
+++ 13 - node/1-global-object/app.js
@@ -0,0 +1,33 @@
+// Global Objects: we can access it anywhere in any file.
+// var - mutable/changeable - global scope
+var varContainer = "Global Scope";
+var varFunction = function () {
+  console.log("Function expression using var keyword");
+};
+// let - mutable/changeable - block scope
+let letContainer = "Block scope";
+// Browser: window object
+
+// console.log("=== ACCESS USING WINDOW OBJECT ===");
+// console.log(window.varContainer);
+// window.varFunction();
+// console.log("=== AUTOMATIC PREFIX ===");
+// console.log(varContainer);
+// varFunction();
+
+// console.log("=== ACCESS USING WINDOW OBJECT BLOCK ===");
+// console.log(window.letContainer); // undefined
+
+// console.log("=== PROBLEM ===");
+// var varContainer = "Updated Global Scope";
+// var varFunction = function () {
+//   console.log("Function expression using var keyword (Updated).");
+// };
+// console.log(window.varContainer);
+// window.varFunction();
+
+// Node.js: global
+console.log("Var:", global.varContainer);
+console.log("Let:", global.letContainer);
+// global.varFunction();
+        
---
+++ 13 - node/2-modules/app.js
@@ -0,0 +1,9 @@
+const lettersModule = require("./letters");
+// import module without export
+// console.log(lettersModule);
+
+// With export
+console.log(lettersModule);
+
+console.log("Letters:", letters);
+displayLetters();
