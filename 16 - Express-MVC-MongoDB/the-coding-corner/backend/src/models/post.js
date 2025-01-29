9c6357c6d7e1f7527ac9001f9a656038803f8915
jovinds <jovinds24@gmail.com>
2025-01-29 19:03:28 +0800

    coding cornner update

---
+++ 16 - Express-MVC-MongoDB/the-coding-corner/backend/src/models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  comments: [{ type: String }],
+});
+
+// _id : Unique identifier for each of the documents.
+// __v : Tracks how many times you updated that document.
+
+const db = mongoose.connection.useDb("blog_db");
+const Post = db.model("Post", postSchema);
+
+module.exports = Post;
